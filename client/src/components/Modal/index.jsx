5b2f89557559e4c54f16704a049b4f7d9f53b3a7
Alferov Dmitriy <alfie49@yandex.ru>
2021-05-09 21:02:38 +0000

    Feature: Modal
    * Компонент Modal (модальное окно)

---
+++ client/src/components/Modal/index.jsx
@@ -0,0 +1,28 @@
+import clsx from 'clsx'
+import { useEffect, memo } from 'react'
+
+import './Modal.sass'
+
+const Modal = ({ children, onClose = () => {}, isVisible = false, className }) => {
+	console.log('MODAL RENDERS')
+	useEffect(() => {
+		isVisible 
+			? document.body.style.overflow = 'hidden'
+			: document.body.style.overflow = 'auto'
+
+		return () => {
+			document.body.style.overflow = 'auto'
+		}
+	}, [isVisible])
+
+	return (
+		<div className={clsx('Modal', className)} onMouseUp={(e) => e.stopPropagation()}>
+			<div className="Modal__overlay" onClick={onClose}></div>
+			<div className="Modal__content">
+				{children}
+			</div>
+		</div>
+	)
+}
+
+export default memo(Modal)
--- client/src/pages/Board/index.jsx
+++ client/src/pages/Board/index.jsx
@@ -4,8 +4,9 @@ import { useDispatch, useSelector } from 'react-redux'
 import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 
 import { fetchBoardById, moveList } from '../../store/reducers/boards'
-import { fetchCards, createList, createCard, moveCard, setLists } from '../../store/reducers/lists'
+import { createList, moveCard } from '../../store/reducers/lists'
 
+import Modal from '../../components/Modal'
 import List from './components/List'
 import AddForm from './components/AddForm'
 import './Board.sass'
@@ -15,25 +16,13 @@ const Board = () => {
 	const dispatch = useDispatch()
 	const listsRef = useRef(null)
 	const [newListMode, setNewListMode] = useState(false)
+   const [modalVisible, setModalVisible] = useState(false)
 	const { currentBoard } = useSelector(state => state.boards)
 	const { items } = useSelector(state => state.lists)
 
 	useEffect(() => {
 		id && dispatch(fetchBoardById(id))
 	}, [id, dispatch])
-
-   // useEffect(() => {
-   //    document.documentElement.style.overflowY = 'hidden'
-   //    return () => {
-   //       document.documentElement.style.overflowY = 'auto'
-   //    }
-   // })
-
-   // useEffect(() => {
-   //    return () => {
-   //       dispatch(setLists([]))
-   //    }
-   // }, [dispatch])
 
 	const scrollToRight = () => {
 		window.scrollTo({
@@ -85,16 +74,16 @@ const Board = () => {
    }
 
 	return (
-      <DragDropContext onDragEnd={onDragEnd}>
-         <>
-            <div className="BoardPage__bg">
-               {currentBoard && <img src={currentBoard.bg} alt="Board bg" />}
-            </div>
+      <>
+         <div className="BoardPage__bg">
+            {currentBoard && <img src={currentBoard.bg} alt="Board bg" />}
+         </div>
 
-            <main className="BoardPage page">
-               <h2 className="BoardPage__title">{currentBoard && currentBoard.title}</h2>
-               <div className="BoardPage__container">
-
+         <main className="BoardPage page">
+            <h2 className="BoardPage__title">{currentBoard && currentBoard.title}</h2>
+            
+            <div className="BoardPage__container">
+               <DragDropContext onDragEnd={onDragEnd}>
                   <div className="BoardPage__lists" ref={listsRef}>
                      <Droppable
                         droppableId="all-lists"
@@ -109,17 +98,13 @@ const Board = () => {
                            >
                               {items && items.length
                                  ? items.map((list, index) => (
-                                      <List
-                                         key={list._id}
-                                         index={index}
-                                         className="BoardPage__list"
-                                         list={list}
-                                       //   getAllCards={fetchCards}
-                                       //   createCard={createCard}
-                                       //   editCard={editCard}
-                                       //   deleteCard={deleteCard}
-                                      />
-                                   ))
+                                    <List
+                                       key={list._id}
+                                       index={index}
+                                       className="BoardPage__list"
+                                       list={list}
+                                    />
+                                 ))
                                  : null}
                               {provided.placeholder}
 
@@ -142,29 +127,190 @@ const Board = () => {
                            </div>
                         )}
                      </Droppable>
-
-                     {/* {!newListMode && (
-                        <div
-                           className="List__addListBtn"
-                           onClick={onNewListMode}
-                        >
-                           + Добавить лист
-                        </div>
-                     )}
-                     {newListMode && (
-                        <div className="BoardPage__addList">
-                           <AddForm
-                              callback={onAddListHandler}
-                              placeholder="Заголовок листа"
-                           />
-                        </div>
-                     )} */}
                   </div>
-               </div>
-            </main>
-         </>
-      </DragDropContext>
+               </DragDropContext>
+            </div>
+
+            {modalVisible && <Modal 
+               isVisible={modalVisible}
+               onClose={() => setModalVisible(false)}
+            >
+               Modal content
+            </Modal>}
+         </main>
+      </>
    )
 }
 
 export default Board
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useRef, useEffect, useState } from 'react'
+// import { useParams } from 'react-router-dom'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+
+// import { fetchBoardById, moveList } from '../../store/reducers/boards'
+// import { createList, moveCard } from '../../store/reducers/lists'
+
+// import Modal from '../../components/Modal'
+// import List from './components/List'
+// import AddForm from './components/AddForm'
+// import './Board.sass'
+
+// const Board = () => {
+// 	const { id } = useParams()
+// 	const dispatch = useDispatch()
+// 	const listsRef = useRef(null)
+// 	const [newListMode, setNewListMode] = useState(false)
+//    const [modalVisible, setModalVisible] = useState(true)
+// 	const { currentBoard } = useSelector(state => state.boards)
+// 	const { items } = useSelector(state => state.lists)
+
+// 	useEffect(() => {
+// 		id && dispatch(fetchBoardById(id))
+// 	}, [id, dispatch])
+
+// 	const scrollToRight = () => {
+// 		window.scrollTo({
+// 			left: listsRef.current.clientWidth,
+// 			behavior: 'smooth',
+// 		})
+// 	}
+
+// 	const onNewListMode = () => {
+// 		scrollToRight()
+// 		setNewListMode(true)
+// 	}
+
+// 	const onAddListHandler = (title) => {
+// 		setNewListMode(false)
+// 		if (!title.length) return
+
+// 		dispatch(createList(id, title))
+// 	}
+
+
+// 	const onDragEnd = (result) => {
+//       const { destination, source, draggableId, type } = result
+
+//       if (!destination) return
+//       if (
+//          destination.droppableId === source.droppableId &&
+//          destination.index === source.index
+//       )
+//          return
+
+//       if (type === 'list') {
+//          // логика для сортировки листов
+//          dispatch(moveList({
+//             boardId: id,
+//             listId: draggableId,
+//             currentPosition: source.index,
+//             newPosition: destination.index,
+//          }))
+//       } else if (type === 'card') {
+//          dispatch(moveCard({
+//             startListId: source.droppableId, // id листа из которого перемещаем карточку
+//             cardId: draggableId, // id перемещаемой карточки
+//             endListId: destination.droppableId, // id листа в который перемещаем карточку
+//             currentPosition: source.index, // индекс текущей перемещаемой карточки
+//             newPosition: destination.index, // индекс новой позиции в массиве
+//          }))
+//       }
+//    }
+
+// 	return (
+//       <DragDropContext onDragEnd={onDragEnd}>
+//          <>
+//             <div className="BoardPage__bg">
+//                {currentBoard && <img src={currentBoard.bg} alt="Board bg" />}
+//             </div>
+
+//             <main className="BoardPage page">
+//                <h2 className="BoardPage__title">{currentBoard && currentBoard.title}</h2>
+//                <div className="BoardPage__container">
+
+//                   <div className="BoardPage__lists" ref={listsRef}>
+//                      <Droppable
+//                         droppableId="all-lists"
+//                         direction="horizontal"
+//                         type="list"
+//                      >
+//                         {(provided) => (
+//                            <div
+//                               {...provided.droppableProps}
+//                               ref={provided.innerRef}
+//                               className="BoardPage__lists-items"
+//                            >
+//                               {items && items.length
+//                                  ? items.map((list, index) => (
+//                                       <List
+//                                          key={list._id}
+//                                          index={index}
+//                                          className="BoardPage__list"
+//                                          list={list}
+//                                       />
+//                                    ))
+//                                  : null}
+//                               {provided.placeholder}
+
+//                               {!newListMode && (
+//                                  <div
+//                                     className="List__addListBtn"
+//                                     onClick={onNewListMode}
+//                                  >
+//                                     + Добавить лист
+//                                  </div>
+//                               )}
+//                               {newListMode && (
+//                                  <div className="BoardPage__addList">
+//                                     <AddForm
+//                                        callback={onAddListHandler}
+//                                        placeholder="Название листа"
+//                                     />
+//                                  </div>
+//                               )}
+//                            </div>
+//                         )}
+//                      </Droppable>
+//                   </div>
+//                </div>
+
+//                {modalVisible && <Modal 
+//                   isVisible={modalVisible}
+//                   onClose={() => setModalVisible(false)}
+//                >
+//                   Modal content
+//                </Modal> }
+//             </main>
+//          </>
+//       </DragDropContext>
+//    )
+// }
+
+// // export default memo(Board)
+// export default Board
