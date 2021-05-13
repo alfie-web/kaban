6526664f357d6e702c421b17dd7ae1f927039304
Alferov Dmitriy <alfie49@yandex.ru>
2021-05-13 00:32:16 +0000

    BugFix: Modal component scroll
    * Теперь можно прокручивать окно скроллбаром

--- client/src/components/Modal/index.jsx
+++ client/src/components/Modal/index.jsx
@@ -1,28 +1,35 @@
 import clsx from 'clsx'
-import { useEffect, memo } from 'react'
+import { useEffect, memo, useRef } from 'react'
 
 import './Modal.sass'
 
 const Modal = ({ children, onClose = () => {}, isVisible = false, className }) => {
-	console.log('MODAL RENDERS')
+	// console.log('MODAL RENDERS')
+
+	const barWidth = useRef(null)
+	const overlayRef = useRef(null)
+	const modalRef = useRef(null)
+
 	useEffect(() => {
-		isVisible 
-			? document.body.style.overflow = 'hidden'
-			: document.body.style.overflow = 'auto'
+		document.body.style.overflow = 'hidden'
+
+		const barW = modalRef.current.offsetWidth - modalRef.current.clientWidth
+		overlayRef.current.style.width = `calc(100% - ${barW}px)`
 
 		return () => {
 			document.body.style.overflow = 'auto'
 		}
-	}, [isVisible])
+	}, [])
 
-	return (
-		<div className={clsx('Modal', className)} onMouseUp={(e) => e.stopPropagation()}>
-			<div className="Modal__overlay fadeIn" onClick={onClose}></div>
+	return isVisible ? (
+		<div ref={modalRef} className={clsx('Modal', className)} onMouseUp={(e) => e.stopPropagation()}>
+			<div ref={overlayRef} className="Modal__overlay fadeIn" onClick={onClose}></div>
 			<div className="Modal__content fadeInUp">
+			{barWidth.current}
 				{children}
 			</div>
 		</div>
-	)
+	) : null
 }
 
 export default memo(Modal)
