90d35cc3f3b77b27687f7abfbd4e8d47e56ea83f
Alferov Dmitriy <52403225+alfie-web@users.noreply.github.com>
2021-05-18 13:06:10 +0000

    Feature: Lists preloader
    * Preloader во время подгрузки очередной порции карточек

---
+++ client/src/pages/Board/components/List/components/Preloader/index.jsx
@@ -0,0 +1,16 @@
+import { memo } from 'react'
+import { useSelector } from 'react-redux'
+
+import Preloader from '../../../../../../components/Preloader'
+
+const ListPreloader = ({ listId }) => {
+	const isCardsFetching = useSelector(state => state.lists.isCardsFetching)
+
+	return isCardsFetching && isCardsFetching === listId ? (
+		<div className="List__preloader">
+			<Preloader />
+		</div>
+	) : null
+}
+
+export default memo(ListPreloader)
