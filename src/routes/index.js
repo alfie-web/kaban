59dcd47daa335c23887e753012a1259cd60fb73d
Alferov Dmitriy <52403225+alfie-web@users.noreply.github.com>
2021-05-19 07:16:47 +0000

    Feature: Flash alerts
    * Поправлено состояние disabled у кнопки
    * Алерт при успешной или не авторизации

--- src/routes/index.js
+++ src/routes/index.js
@@ -30,6 +30,14 @@ module.exports = function createRoutes(app) {
 
    app.use(morgan('dev'))
 
+   app.use(function (req, res, next) {
+      if ('OPTIONS' == req.method) {
+         return res.sendStatus(200)
+      } else {
+         next()
+      }
+   })
+
    app.use('/api/users', userRoutes)
    app.use('/api/boards', boardsRoutes)
    app.use('/api/lists', listsRoutes)
